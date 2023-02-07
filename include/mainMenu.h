5d35cbe8294d739954d4ccdc1734e21300b4febd
varun chandran <64518590+eartquak@users.noreply.github.com>
2023-02-08 04:33:35 +0530

    code is self contained ig

--- include/mainMenu.h
+++ include/mainMenu.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <SDL.h>
+#include <SDL_main.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <stdio.h>
+#include "Vector2.h"
+
 class mainMenu
 {
+public:
+	mainMenu();
+	~mainMenu();
+	void init();
+	void handleEvent();
+	void update();
+	void render();
+private:
+	bool isRunning = false;
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	Vector2 SCREEN_SIZE;
+	Vector2 MAP_SIZE;
 };
 
